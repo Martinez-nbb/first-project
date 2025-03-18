1c4db9cb5be3b36503c947f3969fd2f2c4dc17c3
Makariy Martyanov <makariy06@mail.ru>
2025-03-18 22:52:39 +0400

    feat: add fibonacci function

--- main.cpp
+++ main.cpp
@@ -1,5 +1,12 @@
 #include <iostream>
 using namespace std;
+int f(int n){
+    if (n == 1 || n == 2) return 1;
+    else return f(n - 1) + f(n - 2);
+}
 int main() {
-    cout << "Hello, World!";
+    //cout << "Hello, World!\n";
+    int n;
+    cin >> n;
+    cout << f(n) << '\n';
 }
