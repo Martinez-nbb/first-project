5f7282cd87e40c9b65d08b2c66e05286f18872f7
Makariy Martyanov <makariy06@mail.ru>
2025-03-18 22:59:34 +0400

    feat: change fibonacci function

--- main.cpp
+++ main.cpp
@@ -1,7 +1,8 @@
 #include <iostream>
 using namespace std;
 int f(int n){
-    if (n == 1 || n == 2) return 1;
+    if (n == 1) return 0;
+    else if (n == 2) return 1;
     else return f(n - 1) + f(n - 2);
 }
 int main() {
