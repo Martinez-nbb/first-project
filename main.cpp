21643bf19a28f810e4cdfea56df1122710e5ca17
Makariy Martyanov <makariy06@mail.ru>
2025-04-24 19:40:37 +0400

    fix: add iterative calculation

--- main.cpp
+++ main.cpp
@@ -1,15 +1,12 @@
 #include <iostream>
+#include <vector>
 using namespace std;
-int f(int n){
-    if (n == 1) return 0;
-    else if (n == 2) return 1;
-    else return f(n - 1) + f(n - 2);
-}
 int main() {
-    //cout << "Hello, World!\n";
     int n;
     cin >> n;
-    for (int i = 1; i <= n; ++i) {
-        cout << f(i) << '\n';
-    }
+    vector<int>  f(n + 1);
+    f[0] = 0;
+    f[1] = 1;
+    for (int i = 2; i <= n; ++i) f[i] = f[i - 1] + f[i - 2];
+    for (int i = 0; i < n + 1; ++i) cout << f[i] << '\n';
 }
